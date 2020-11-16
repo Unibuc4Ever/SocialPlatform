757c03f86e53559b5d0de549e7c4771d56d14199
Felix Puscasu <puscasu.felix1@gmail.com>
2020-11-16 17:26:03 +0200

    More work

--- SocialPlatform/SocialPlatform/Controllers/UsersController.cs
+++ SocialPlatform/SocialPlatform/Controllers/UsersController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using SocialPlatform.Models;
 
 namespace SocialPlatform.Controllers
@@ -10,10 +12,46 @@ namespace SocialPlatform.Controllers
     public class UsersController : Controller
     {
         private static ApplicationDbContext db = new ApplicationDbContext();
+        private static UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(new
+            UserStore<ApplicationUser>(db));
         // GET: Users
         public ActionResult Index()
         {
             return View(db.Users);
+        }
+
+        // GET: MakeFriendship
+        [Authorize]
+        public ActionResult FriendRequest()
+        {
+            return View(new FriendRequest());
+        }
+
+        // POST: MakeFriendship
+        [HttpPost]
+        [Authorize]
+        public ActionResult FriendRequest(FriendRequest fr)
+        {
+            ApplicationUser user = userManager.FindById(User.Identity.GetUserId());
+            try
+            {
+                if (ModelState.IsValid) {
+                    // ApplicationUser other = userManager.FindById(fr.otherID);
+                    
+                    user.friendRequests.Add(db.Users.Find(fr.otherID));
+                    
+                    TempData["message"] = "Now I have " + user.friendRequests.Count + " friend requests";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(fr);
+                }
+            }
+            catch (Exception)
+            {
+                return View(fr);
+            }
         }
     }
 }
