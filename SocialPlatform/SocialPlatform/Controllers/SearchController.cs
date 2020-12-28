c7531a0245334171e232d7aa79e4a943967e7fa9
Felix Puscasu <puscasu.felix1@gmail.com>
2020-12-28 12:17:18 +0200

    Backend for pagination

--- SocialPlatform/SocialPlatform/Controllers/SearchController.cs
+++ SocialPlatform/SocialPlatform/Controllers/SearchController.cs
@@ -40,5 +40,88 @@ namespace SocialPlatform.Controllers
 
             return View();
         }
+
+        public ActionResult PagedGroups(string query, int cnt, int lastWallID)
+        {
+            var db = new ApplicationDbContext();
+            db.Configuration.LazyLoadingEnabled = false;
+            db.Configuration.ProxyCreationEnabled = false;
+
+            var groups = db.Groups.Where(group =>
+                group.Name.Contains(query)).ToList();
+
+            groups.Sort(delegate (Group a, Group b) {
+                return (a.GroupId < b.GroupId) ? -1 : 1;
+            });
+
+            // If not found, returns -1, but we want to anyway not return the old
+            // Element, so we add 1
+            var index = groups.FindIndex(gr => gr.WallId == lastWallID) + 1;
+            cnt = Math.Min(cnt, groups.Count() - index);
+
+            return Json(
+                new { 
+                    groups = groups.GetRange(index, cnt) 
+                }, 
+                JsonRequestBehavior.AllowGet
+            );
+        }
+
+        public ActionResult PagedUsers(string query, int cnt, int lastWallID)
+        {
+            var db = new ApplicationDbContext();
+            db.Configuration.LazyLoadingEnabled = false;
+            db.Configuration.ProxyCreationEnabled = false;
+
+            var users = db.Users.Where(user =>
+                (user.FirstName + " " + user.LastName).Contains(query) ||
+                (user.LastName + " " + user.FirstName).Contains(query)).ToList();
+
+            users.Sort(delegate (ApplicationUser a, ApplicationUser b)
+            {
+                return (a.WallId < b.WallId) ? -1 : 1;
+            });
+
+            // If not found, returns -1, but we want to anyway not return the old
+            // Element, so we add 1
+            int index = users.FindIndex(usr => usr.WallId == lastWallID) + 1;
+            cnt = Math.Min(cnt, users.Count() - index);
+
+            return Json(
+                new { 
+                    users = users.GetRange(index, cnt) 
+                },
+                JsonRequestBehavior.AllowGet
+            );
+        }
+
+        public ActionResult PagedPosts(string query, int cnt, int lastWallID)
+        {
+            var db = new ApplicationDbContext();
+            db.Configuration.LazyLoadingEnabled = false;
+            db.Configuration.ProxyCreationEnabled = false;
+
+            var posts = db.Posts.Where(post =>
+                post.Title.Contains(query) ||
+                post.Content.Contains(query)).ToList();
+
+            posts.Sort(delegate (Post a, Post b)
+            {
+                return (a.WallId < b.WallId) ? -1 : 1;
+            });
+
+            // If not found, returns -1, but we want to anyway not return the old
+            // Element, so we add 1
+            int index = posts.FindIndex(usr => usr.WallId == lastWallID) + 1;
+            cnt = Math.Min(cnt, posts.Count() - index);
+
+            return Json(
+                new { 
+                    users = posts.GetRange(index, cnt) 
+                },
+                JsonRequestBehavior.AllowGet
+            );
+           
+        }
     }
 }
