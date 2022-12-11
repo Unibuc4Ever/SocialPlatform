73f7c6ba813afd4a05c09edf2e4c13dfb430560d
Felix Puscasu <puscasu.felix1@gmail.com>
2022-12-11 14:15:40 +0200

    First modified UserModel, and added migration for firstName

---
+++ SocialPlatform/SocialPlatform/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SocialPlatform.Models;
+
+namespace SocialPlatform.Controllers
+{
+    public class UsersController : Controller
+    {
+        private static ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(db.Users);
+        }
+    }
+}
