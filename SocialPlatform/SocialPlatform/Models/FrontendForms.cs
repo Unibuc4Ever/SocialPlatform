06178c3c4a646d64ff45fb2f43b88162d8c160f7
Felix Puscasu <puscasu.felix1@gmail.com>
2022-12-11 14:15:41 +0200

    More work

---
+++ SocialPlatform/SocialPlatform/Models/FrontendForms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SocialPlatform.Models
+{
+    public class FriendRequest
+    {
+        [Required(ErrorMessage = "Cu cine vrei sa fii priete? Pune ID")]
+        public string otherID { get; set; }
+    }
+}
